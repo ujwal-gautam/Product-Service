e846b53d1238365cab4375551ee804e920ec728f
vanshu-samrutwar <vanshu24june@gmail.com>
2025-03-22 19:30:52 +0530

    init commit

---
+++ src/main/java/spring/productservice/controller/ProductController.java
@@ -0,0 +1,16 @@
+package spring.productservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @GetMapping("/{id}")
+    public String getProduct(@PathVariable String id) {
+        return "Product ID: " + id;
+    }
+}
